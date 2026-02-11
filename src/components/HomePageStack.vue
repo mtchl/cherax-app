8bdab1ac152fa85ecd2eacb7b8d48f55f62a5994
mtchl <mitchell.whitelaw@gmail.com>
2026-02-11 11:17:57 +1100

    Rebuilding scrolly narrative

--- src/components/HomePageStack.vue
+++ src/components/HomePageStack.vue
@@ -16,14 +16,20 @@
             hidden:false,
             animate:null,
             // currentAnimation:null
-            triggers: {
-                spoonbillHenSnag:false, 
-                spearMorsel:false,
-                greatCormorant:false,
-                footStir:false,
-                spoonbillHunting:false,
-                squabbleFrog:false
-            }
+            // triggers: {
+            //     spoonbillHenSnag:false, 
+            //     spearMorsel:false,
+            //     greatCormorant:false,
+            //     footStir:false,
+            //     spoonbillHunting:false,
+            //     squabbleFrog:false,
+            //     kiteTussle:false,
+            //     swampHarrierPaddock:false,
+            //     swampHarrierSettling:false,
+            // },
+            triggers:{},
+            animations:{},
+            progress:{}
          }
       },
 
@@ -33,29 +39,33 @@
             if (element.dataset.playtrigger){
                 this.triggers[element.dataset.playtrigger] = true;
                 console.log("playtrigger " + element.dataset.playtrigger)
-            }
-            // if (element.dataset.url) this.currentSlide = element.dataset;
-            
-            // if (element.dataset.animate) {
-            //    this.animate = element.dataset.animate; 
-            //    console.log(this.animate)
-            // } else {
-            //    this.animate = null
-            // }
 
-            // if (element.dataset.hide) {
-            //    this.hidden = true
-            // } else {
-            //    this.hidden = false
-            // }
+            }
+            // stash the animation class in the animations object
+            if (element.dataset.animate){
+                this.animations[element.dataset.animtarget] = element.dataset.animate;
+                console.log(element.dataset.animate)
+            }
+
+
+
           },
 
           stepProgressHandler({ element, progress, index }){
-            //console.log("step-progress", { element, progress, index });
+            if (element.dataset.progress){
+                this.progress[element.dataset.progress] = progress;
+                console.log(progress)
+            }
           },
 
-          stepExitHandler({ element, index }){
+          stepExitHandler({ element, index, direction }){
             //console.log("step exit", {element, index})
+
+            if (direction == "up" && element.dataset.playtrigger){
+                if (this.triggers[element.dataset.playtrigger]){
+                    this.triggers[element.dataset.playtrigger] = false;
+                }
+            }
           }
       }
    }
@@ -91,9 +101,9 @@
                  At dawn, a <router-link :to="'/captures?species=pelican'">pelican</router-link> touches down at the edge of the flooded Crop Paddock 
             </div>
 
-            <ScrollyImg class="step" type="video" :playtrigger="triggers.spoonbillHenSnag" src="https://storage.googleapis.com/cherax-media/cam7/cam7-20241109-072636.mp4"></ScrollyImg>
+            <ScrollyImg class="step" type="video" data-playtrigger="spoonbillHenSnag" :playtrigger="triggers.spoonbillHenSnag" src="https://storage.googleapis.com/cherax-media/cam7/cam7-20241106-103414.mp4"></ScrollyImg>
 
-            <div class="story step" data-playtrigger="spoonbillHenSnag">
+            <div class="story" >
                     A <router-link :to="'/captures?species=yellow-billed-spoonbill'">yellow-billed spoonbill</router-link> and a flock of <router-link :to="'/captures?species=native-hen'">native hens</router-link> feed around the nearby snag... 
             </div>
 
@@ -170,6 +180,71 @@
         </section>
 
 
+        <section>
+
+            <div class="section-head nomargin">
+                  <h3>3. Raptors</h3>
+            </div>
+
+            <ScrollyImg type="img" src="https://storage.googleapis.com/cherax-media/cam7/cam7-20241230-155324.JPG" :animate="animations.hidingKite"></ScrollyImg>
+
+            <div class="story step" data-animate="reset" data-animtarget="hidingKite">
+                As well as waterbirds, wetlands like this are havens for birds of prey 
+            </div>
+
+             <div class="step story" data-animate="zoom-200-right" data-animtarget="hidingKite">
+                <p>Hiding in this frame is a <router-link :to="'/captures?species=whistling-kite'">whistling kite,</router-link> an Australian raptor often found near water.</p> 
+            </div>
+
+            <ScrollyImg type="img"  src="https://storage.googleapis.com/cherax-media/cam3/cam3-20250109-105800.JPG" :animate="animations.fingersKite"></ScrollyImg>
+
+            <div class="step story" data-animate="reset" data-animtarget="fingersKite">
+                This capture from <router-link :to="'/captures?cam=A'">Camera A</router-link> shows the distinctive ‘fingers’ on the whistling kite’s wingtips  
+            </div>
+
+            <div class="step story" data-animate="zoom-200-left" data-animtarget="fingersKite">
+               Whistling kites are named for their piercing call. [Listen] here, or find it in the <router-link :to="'/timeline?species=whistling-kite'">Timeline</router-link>
+            </div>
+
+            <ScrollyImg type="video" src="https://storage.googleapis.com/cherax-media/cam3/cam3-20241219-063226.mp4" :playtrigger="triggers.kiteTussle" :progress="progress.kiteTussle"></ScrollyImg>
+
+            <div class="step story" data-playtrigger="kiteTussle">
+              <p>This video finds two <router-link :to="'/captures?species=whistling-kite'">kites</router-link> tussling over their catch – perhaps a crayfish.</p>       
+            </div>
+
+            <div class="story no-bottom-margin">
+                <p><strong>Scroll</strong> to see the moment the crayfish splashes down to safety</p>
+            </div>
+
+            <div class="step scroller" data-progress="kiteTussle"></div> 
+
+             <ScrollyImg type="img" src="https://storage.googleapis.com/cherax-media/cam4/cam4-20250222-185740.JPG"></ScrollyImg>
+
+              <div class="story">
+                We also spotted a <router-link :to="'/captures?species=swamp-harrier'">swamp harrier,</router-link> another raptor often found in wetlands  
+            </div>
+
+            <ScrollyImg type="video" src="https://storage.googleapis.com/cherax-media/cam4/cam4-20250222-185752.mp4" :playtrigger="triggers.swampHarrierPaddock"></ScrollyImg>
+
+            <div class="step story" data-playtrigger="swampHarrierPaddock">
+              Here it is on the drying edges of the Crop Paddock wetland, in February 
+            </div>
+
+            <ScrollyImg type="video" src="https://storage.googleapis.com/cherax-media/narrative/swamp-harrier-nest-standing.mov" :playtrigger="triggers.swampHarrierNesting"></ScrollyImg>
+
+            <div class="step story" data-playtrigger="swampHarrierNesting">
+              Landholder Peter Morton shared this video of a <router-link :to="'/captures?species=swamp-harrier'">swamp harrier</router-link> nesting in reeds
+            </div>
+
+            <ScrollyImg type="video" src="https://storage.googleapis.com/cherax-media/narrative/swamp-harrier-nest-settling.mov" :playtrigger="triggers.swampHarrierSettling"></ScrollyImg>
+
+            <div class="step story" data-playtrigger="swampHarrierSettling">
+              Peter's use of automated cameras to document the wetlands on his property was the inspiration for this project. 
+            </div>
+
+        </section>
+
+
 
      </VueScrollama>
     </div>
@@ -201,12 +276,14 @@
       z-index:1;
    }
 
-   
-
    .story, .story p{
       font-family: Lato, sans-serif;
       font-size: 1.3rem;
       line-height: 2rem;
+   }
+
+   .story.no-bottom-margin{
+    margin-bottom:0;
    }
 
    .step:first-of-type{
@@ -250,6 +327,12 @@
 
    .story.step.final{
     margin-bottom:100vh;
+   }
+
+   .scroller{
+    height:50vh;
+    background-color: rgba(0,0,0,0.2);
+    width:10px;
    }
 
 
--- src/components/ScrollyImg.vue
+++ src/components/ScrollyImg.vue
@@ -4,7 +4,7 @@
 
 		<div class="outer-wrapper">
 			<div class="slide">
-				<img v-if="type == 'img'" :src="src" />
+				<img v-if="type == 'img'" :src="src" :class="[animate]"/>
 				<video v-if="type == 'video'" :src="src" loop muted ref="vid" />
 			</div>
 		</div>
@@ -15,7 +15,7 @@
 export default {
 
   name: 'ScrollyImg',
-  props: ['src','type','playtrigger'],
+  props: ['src','type','playtrigger','animate','progress','videoScroll'],
 
   data () {
     return {
@@ -31,9 +31,18 @@ export default {
   	playtrigger(newvalue,oldvalue){
   		if (newvalue){ 
   			this.$refs.vid.play()
-  			console.log("watched trigger")
+  			console.log("watched trigger play")
+  		} else {
+  			this.$refs.vid.pause()
+  			this.$refs.vid.currentTime = 0;
+  			console.log("watched trigger pause reset")
   		}
 
+  	},
+
+  	progress(newvalue,oldvalue){
+  		if (!this.$refs.vid.paused) this.$refs.vid.pause();
+  		this.$refs.vid.currentTime = newvalue*this.$refs.vid.duration*0.2;
   	}
 
   }
@@ -61,7 +70,7 @@ export default {
 	    height: auto;
 	    max-height:90vh;
 
-	  	transition: opacity 1.5s ease-out;
+/*	  	transition: opacity 1.5s ease-out;*/
 
 		border: 1px solid black;
 		box-shadow: 0px 0px 4rem 0rem rgba(0,0,0,0.2);
@@ -86,7 +95,15 @@ export default {
 
 	.slide img{
 		aspect-ratio: 4 / 2.8;
+		transition: all 2s ease-out;
+	}
 
+	img.zoom-200-right{
+		transform:scale(2.5) translate(-25%,5%);
+	}
+
+	img.zoom-200-left{
+		transform:scale(2.5) translate(10%,5%);
 	}
 
 
